ab2239a3d00f115694ff71852fe9315ad75b92f1
dgucc <106461038+dgucc@users.noreply.github.com>
2024-11-14 15:05:40 +0100

    Create screenshot.py : save_full_page_screenshot

---
+++ selenium/screenshot.py
@@ -0,0 +1,25 @@
+# Python program to demonstrate
+# python -m pip install selenium
+# https://github.com/mozilla/geckodriver/releases + adapt PATH for geckodriver.exe
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.by import By
+import time
+
+
+options = webdriver.FirefoxOptions()
+options.headless = True
+driver = webdriver.Firefox(options=options)
+
+url = "https://scrapingcourse.com/ecommerce/"
+png = "example.png"
+
+driver.get(url)
+time.sleep(3)
+
+driver.save_full_page_screenshot("img/" + png)
+
+
+driver.close()
+
+driver.quit()
